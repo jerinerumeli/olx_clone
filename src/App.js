10177e1595ed59f458ca806aa3c0da3b8da0d94b
jerin <paperpencil987@gmail.com>
2024-02-21 12:01:36 +0530

    styles imported

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
+import React from 'react';
 import './App.css';
-import { collection, getDocs,addDoc,deleteDoc,doc,updateDoc } from "firebase/firestore"; 
-import {DB,Auth} from "./Firebase/config"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     
     <div >
-      
-
-      <button onClick={()=>{
-        // createUserWithEmailAndPassword(Auth, "jerin@gmail.com", "password")
-        // .then((userCredential) => {
-        //   // Signed up 
-        //   const user = userCredential.user;
-        //   console.log(user)
-        //   // ...
-
-        // })
-        // .catch((error) => {
-        //   const errorCode = error.code;
-        //   const errorMessage = error.message;
-        //   // ..
-        // });
-        
-
-        // updateDoc(doc(DB, "product", "jPrvF22pPblBwkI8liWZ"),{
-        //   "type":"jerin"
-        // });
-
-        getDocs(collection(DB, "product")).then((snapshot)=>{
-          snapshot.forEach((obj)=>{
-            console.log(`${obj.id} => ${JSON.stringify(obj.data())}`)
-          })
-        })
-
-        // addDoc(collection(DB, "product"), {
-        //   "type":"nokia",
-        //   "price":20000
-        // })
-      }}>click</button>
+      <Home />
     </div>
   );
 }
